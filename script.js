444341eccc87c7c07a2e5d3d85f6120fbcfff103
Akshitha Ravi <184779120+Abnarain17@users.noreply.github.com>
2025-06-20 15:43:10 -0500

    Changes made to index, script and style files

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // Optional: You can add interactive features here later
 console.log("Portfolio loaded for Jane Doe.");
+function toggleProject(element) {
+  const card = element.parentElement;
+  card.classList.toggle("active");
+}
